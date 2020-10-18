95dcc04ed3b552eafce0b71bad2fa4b8a70bece0
Kia Storm <kia@kormsen.io>
2020-10-18 16:02:29 +0100

    add netlifyy identity

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Container, Flex, Heading, Button } from "@theme-ui/components";
+import netlifyIdentity from "netlify-identity-widget";
 
-export default (props) => (
-  <div>
-    <h1>Hello</h1>
-  </div>
-);
+export default (props) => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
+
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", p: 3 }}>
+        <Heading as="h1" sx={{ mb: 3 }}>
+          Get Shit Done
+        </Heading>
+        <Button onClick={() => netlifyIdentity.open()} sx={{ mb: 3 }}>
+          Get started
+        </Button>
+        <Button onClick={() => console.log(netlifyIdentity.currentUser())}>
+          Log current user
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
